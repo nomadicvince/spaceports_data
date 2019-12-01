13af31856198380a59fbc51c9ed17e37e6b7fdcf
Vincent Moore <vmoore@cosmiq.cloud>
2019-11-30 20:40:13 -0500

    Add files via upload

---
+++ darkskies.js
@@ -0,0 +1,123 @@
+const DarkSkies = [    
+    {
+      "name": "Beverly Shores",
+      "city": "Beverly Shores",
+      "state": "Indiana",
+      "country": "United States",
+      "position": [-86.985556, 41.684167],
+      "description": "Beverly Shores is a town in Pine Township, Porter County, Indiana, United States, about 36 miles (58 km) east of downtown Chicago.",
+      "Type": "Community",
+      "Elevation": "620 ft",
+      "Designation": "2014",
+      "images": {
+        "main": "photo1",
+        "sidebar": "photo2"
+      },
+      "website": "http://www.beverlyshoresindiana.org/",
+      "radius": 50000
+    },
+    {
+      "name": "Big Park / Village of Oak Creek",
+      "city": "Oak Creek",
+      "state": "Arizona",
+      "country": "United States",
+      "position": [-111.761111, 34.783056],
+      "description": "Big Park is more commonly known as the Village of Oak Creek (VOC), and is a bedroom community for Sedona, Arizona, just 7 miles away.",
+      "Type": "Community",
+      "Elevation": "4,104 ft",
+      "Designation": "2016",
+      "images": {
+        "main": "photo1",
+        "sidebar": "photo2"
+      },
+      "website": "http://www.vocaonline.com/",
+      "radius": 50000
+    },
+    {
+      "name": "Bon Accord",
+      "city": "Bon Accord",
+      "state": "Alberta",
+      "country": "Canada",
+      "position": [-113.418889, 53.832778],
+      "description": "Bon Accord is a town in central Alberta, Canada. It is located 40 km (25 mi) north of downtown Edmonton on Highway 28.",
+      "Type": "Community",
+      "Elevation": "2,051 ft",
+      "Designation": "2016",
+      "images": {
+        "main": "photo1",
+        "sidebar": "photo2"
+      },
+      "website": "https://www.bonaccord.ca/",
+      "radius": 50000
+    },
+    {
+      "name": "Borrego Springs",
+      "city": "Borrego Springs",
+      "state": "California",
+      "country": "United States",
+      "position": [-116.371944, 33.247222],
+      "description": "Borrego Springs is a census-designated place (CDP) in San Diego County, California. There are no stop lights in Borrego Springs and nighttime lighting is kept to a minimum to protect the views of the night sky.",
+      "Type": "Community",
+      "Elevation": "597 ft",
+      "Designation": "2009",
+      "images": {
+        "main": "photo1",
+        "sidebar": "photo2"
+      },
+      "website": "http://www.abdnha.org/borregodarksky/index.htm",
+      "radius": 50000
+    },
+    {
+      "name": "Camp Verde",
+      "city": "Camp Verde",
+      "state": "Arizona",
+      "country": "United States",
+      "position": [-111.856111, 34.566667],
+      "description": "Camp Verde is a town in Yavapai County, Arizona, United States.",
+      "Type": "Community",
+      "Elevation": "3,147 ft",
+      "Designation": "2018",
+      "images": {
+        "main": "photo1",
+        "sidebar": "photo2"
+      },
+      "website": "http://www.campverde.az.gov/",
+      "radius": 50000
+    },
+    {
+      "name": "Coll",
+      "city": "",
+      "state": "",
+      "country": "Scotland",
+      "position": [-6.557222, 56.633333],
+      "description": "Coll lies among the Inner Hebrides about 10 km west of coastal Argyll and hosts just over 200 permanent residents.",
+      "Type": "Community",
+      "Elevation": "348 ft",
+      "Designation": "2013",
+      "images": {
+        "main": "photo1",
+        "sidebar": "photo2"
+      },
+      "website": "http://darkskycoll.co.uk/",
+      "radius": 50000
+    },
+    {
+      "name": "Dripping Springs",
+      "city": "Dripping Springs",
+      "state": "Texas",
+      "country": "United States",
+      "position": [-98.085278, 30.191944],
+      "description": "Dripping Springs is a city in Hays County, Texas, United States, west of Austin, the state capital. The population was 1,788 at the 2010 census, and had risen to an estimated 3,140 ",
+      "Type": "Community",
+      "Elevation": "1,148 ft",
+      "Designation": "2014",
+      "images": {
+        "main": "photo1",
+        "sidebar": "photo2"
+      },
+      "website": "http://www.cityofdrippingsprings.com/",
+      "radius": 50000
+    },
+  ]
+
+ export default DarkSkies;
